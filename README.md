be85338ffa1f96e70be6efde87c5f64838b2f162
Wang Lei <1327578214@qq.com>
2021-06-09 17:09:11 +0800

    提交项目

--- README.md
+++ README.md
@@ -4,3 +4,23 @@
 未得在线商城系统
 开发人：why.js项目组
 框架：VUE + Element UI + vuex等等
+
+## Project setup
+```
+npm install
+```
+
+### Compiles and hot-reloads for development
+```
+npm run serve
+```
+
+### Compiles and minifies for production
+```
+npm run build
+```
+
+### Lints and fixes files
+```
+npm run lint
+```
---
+++ src/router/index.js
@@ -0,0 +1,48 @@
+import Vue from 'vue';
+import VueRouter from 'vue-router';
+import Home from '@/components/Home';
+import Login from "@/components/Login";
+import Register from "@/components/Register";
+import Content from "@/components/Content";
+
+Vue.use(VueRouter)
+
+const routes = [
+    {
+        path: '/',
+        name: 'Index',
+        component: Home
+    },
+    {
+        path: '/login',
+        name: 'Login',
+        component: Login
+    },
+    {
+        path: '/register',
+        name: 'Register',
+        component: Register
+    },
+    {
+        path: '/content',
+        name: 'Content',
+        component: Content,
+        meta:{requireAuth:true}
+    },
+    {
+        path: '/home',
+        name: 'Home',
+        component: Home
+    }
+];
+
+const router = new VueRouter({
+    // mode: 'history',
+    // base: process.env.BASE_URL,
+    routes: routes,
+    mode: 'history'
+})
+
+
+
+export default router
